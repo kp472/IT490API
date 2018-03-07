fe8d024251e1035e50dfda3ad14a650b63ec3703
kp472 <kp472@njit.edu>
2018-03-07 00:41:56 -0500

    added mcrypt-encryption

--- apifunctions.php
+++ apifunctions.php
@@ -6,13 +6,21 @@ require_once('rabbitMQLib.inc');
 
 function searchLocation($location){
   ini_set("allow_url_fopen", 1);
+  define(MCRYPT_RIJNDAEL_256,false);
   //$uid = $_GET['uid'];
   $url = "https://api.betterdoctor.com/2016-03-01/doctors?location=$location&skip=0&limit=10&user_key=d6fb865f0d167679bbe87e722ea09bdc";
   //echo $url;
   //echo $json;
   $data = file_get_contents($url);
   echo $data;
-  return $data;
+  $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+
+
+  $encrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_ECB);
+
+  $val = base64_encode($encrypt);
+  echo $val;
+  return $val;
 }
 
 function search($uid){
