25e40754f86a3e8deb04fa96d5bc8322194bcd7c
kp472 <kp472@njit.edu>
2018-03-07 20:07:15 -0500

    added functions

--- apifunctions.php
+++ apifunctions.php
@@ -40,11 +40,29 @@ function search($uid){
   echo $val;
   return $val;
 }
-function doctorSpeciality($speciality,$location){
+function doctorSpeciality($location, $speciality){
 
   ini_set("allow_url_fopen", 1);
   //$uid = $_GET['uid'];
   $url = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=$speciality&location=$location&skip=0&limit=10&user_key=d6fb865f0d167679bbe87e722ea09bdc";
+  //echo $url;
+  //echo $json;
+  $data = file_get_contents($url);
+  echo $data;
+  $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+
+
+  $encrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_ECB);
+
+  $val = base64_encode($encrypt);
+  echo $val;
+  return $val;
+}
+function doctorInsurance($location, $insurance){
+
+  ini_set("allow_url_fopen", 1);
+  //$uid = $_GET['uid'];
+  $url = "https://api.betterdoctor.com/2016-03-01/doctors?insurance_uid=$insurance&location=$location&skip=0&limit=10&user_key=d6fb865f0d167679bbe87e722ea09bdc";
   //echo $url;
   //echo $json;
   $data = file_get_contents($url);
@@ -78,11 +96,12 @@ function requestProcessor($request)
       return doValidate($request['sessionId']);
     case "location":
       return searchLocation($request['location']);
-    case "search":
+    case "uid":
       return search($request['uid']);
-    case "practices":
-      return doctorSpeciality($request['speciality'],$request['location']);
-
+    case "speciality":
+      return doctorSpeciality($request['location'],$request['speciality']);
+      case "insurance":
+        return doctorInsurance($request['location'],$request['insurance']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
